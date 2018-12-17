38f7db7b72e708534b6af30a8671d01f06f4a454
abyanjan <43270035+abyanjan@users.noreply.github.com>
2018-12-17 16:36:49 +0200

    Update Final-codes.R

--- Final-codes.R
+++ Final-codes.R
@@ -1,7 +1,9 @@
-# 1. Import the libraries and Data ----
+
+# 1. Import Libraries ----
 
 library(tidyverse)
 library(dplyr)
+library(lubridate)
 library(car)
 library(caret)
 library(corrplot)
@@ -71,6 +73,13 @@ survival_data <- bind_rows(time_1,time_2,time_3,time_4)
 
 survival_data <- survival_data[sample(1:nrow(survival_data)),]
 
+
+# Remove loans before 2013
+
+survival_data <- survival_data %>% 
+  mutate(Year = year(LoanDate)) %>% 
+  filter(Year > 2012 ) 
+
 summary(survival_data)
 
 # Removing observations with  0 values in EmploymentStatus variable and replacing -1 with 1 in 
@@ -82,52 +91,49 @@ survival_data <- survival_data %>%
          UseOfLoan = if_else(UseOfLoan == -1, 1L, UseOfLoan),
          MaritalStatus = if_else(MaritalStatus == -1, 1L, MaritalStatus))
 
+# checking for the missing values
+
+colSums(is.na(survival_data))
+
+# Remove NAs in Rating, Time and LossGivenDefault
+
+survival_data <- survival_data %>% 
+  drop_na(c(Rating,Time, LossGivenDefault))
+
+
+
 # Binnig the Income to categorica variable
 
 survival_data$IncomeTotal <- bin(survival_data$IncomeTotal, nbins = 3, 
                                  labels = c('low','middle','high'),
                                  method = 'content')
 
-# checking for the missing values
-
-colSums(is.na(survival_data))
-
-# Replacing NAs in Rating as Missing
-
-survival_data$Rating <- ifelse(is.na(survival_data$Rating), 'Missing', survival_data$Rating)
 
 # Changing categorical variables to factors
 
 survival_data <- survival_data %>% 
   mutate_at(vars(NewCreditCustomer, VerificationType, Gender, Country, UseOfLoan, MaritalStatus,
-                 EmploymentStatus, IncomeTotal, Rating, State), as.factor)
-
-
-# checking NAs
-colSums(is.na(survival_data))
-
-# Removing observations with NAs in Time variable
-survival_data <- survival_data[!is.na(survival_data$Time),]
+                 EmploymentStatus, Rating, State,Year), as.factor)
 
 
 # 3. Kaplan-Meier test ----
 
 # Kaplan-Meier overall survival curve
 
-Kmcurve <- survfit(Surv(Time,Status)~ 1, data =survival_data)
+Kmcurve <- survfit(Surv(Time,Status)~ Rating, data = survival_data)
 print(Kmcurve)
 summary(Kmcurve)
 
 # kaplan-meier survival curve plot
 
 ggsurvplot(Kmcurve,
-           pval = TRUE, conf.int = TRUE, conf.int.style = 'step',
-           risk.table = TRUE, # Add risk table
+           pval = F, conf.int = TRUE, conf.int.style = 'step',
+           risk.table = FALSE, # Add risk table
            surv.median.line = "hv",# Specify median survival
-           tables.height = 0.20,
            title = "Kaplan Meier Survival Curve",
-           ggtheme = theme_survminer(),
-           palette = "#FIE345")
+           ggtheme = theme_minimal(),
+           palette = "#GHI343",
+           legend = 'bottom')
 
 
 
@@ -184,36 +190,42 @@ test_data$hazard  <- 1/predict(exp_model,newdata = test_data, type='response')
 
 get_evaluation_data <- function(data){
   
-  # filter data with missing values in LossGivenDefault
-  data <- data[which(!is.na(data$LossGivenDefault)),]
-  
   # Prepare empty data frame to store the data
-  evaluation_data <- data.frame(Period = numeric(0), Actual = numeric(0),
-                                Prediction = numeric(0))
+  evaluation_data <- data.frame(Period = numeric(0), Actual = numeric(0),Hazard = numeric(0),
+                                I = numeric(0), D = numeric(0), observed_profit = numeric(0))
+  
+  # convert annual Interest rates to monthly interest rate and express in decimal
+  data$Interest = (data$Interest / 12) / 100
   
   # generate required data                             
   for (i in 1:nrow(data)){
     
-    #generate hazard probabilities
-    hazard_prob <- rep(data$hazard[i],data$Time[i])
-    
     #generate time periods
     t <- seq(1, data$Time[i], by=1)
     
-    #generate survival probabilities
-    surv_prob <- exp(- hazard_prob * t)
+    #generate hazard probabilities
+    Hazard <- rep(data$hazard[i],data$Time[i])
     
-    # generate actual status
+    # generate interest rates
+    I <- rep(data$Interest[i], data$Time[i])
+    
+    # generate Loss given defaults
+    D <- - rep(data$LossGivenDefault[i],data$Time[i])
+    
+    # generate actual status and observed profit
     
     if (data$Status[i] == 1) {
       actual_status <- c(rep(0, (data$Time[i]- 1)), 1)
+      observed_profit <- c(rep(data$Interest[i], (data$Time[i]- 1)), - (data$LossGivenDefault[i]))
       
     } else {
       actual_status <- rep(0, data$Time[i])
+      observed_profit <- rep(data$Interest[i], data$Time[i])
     }
     
     # create temporary data frame to store the generated data
-    df <- data.frame(Period = t, Actual = actual_status, Prediction = surv_prob)
+    df <- data.frame(Period = t, Actual = actual_status, Hazard = Hazard, I = I, D = D,
+                     observed_profit = observed_profit)
     
     evaluation_data <- rbind(evaluation_data, df)
   }
@@ -224,15 +236,16 @@ get_evaluation_data <- function(data){
 
 # for train data
 
-folds_train <- split(train_data, floor(seq(1,20, length.out = nrow(train_data))))
+folds_train <- split(train_data, floor(seq(1,30, length.out = nrow(train_data))))
 
 train_evaluation_data <- map_df(.x = folds_train, .f = get_evaluation_data)
 
 # for test data
 
-folds_test <- split(test_data, floor(seq(1,5, length.out = nrow(test_data))))
+folds_test <- split(test_data, floor(seq(1,10, length.out = nrow(test_data))))
 
 test_evaluation_data <- map_df(.x = folds_test, .f = get_evaluation_data)
+
 
 # calculate mean square prediction error for test set
 
@@ -246,9 +259,8 @@ mse_evaluation <- train_evaluation_data %>%
   # rowbind train and test evaluation data
   bind_rows(test_evaluation_data) %>% 
   
-  # calculate default probability and squared error
-  mutate(default_risk = 1 - Prediction,
-         squared_error = (Actual - default_risk)^2) %>%
+  # calculate  squared error of default prediction
+  mutate(squared_error = (Actual - Hazard)^2) %>%
   
   # group by train and test data
   group_by(id) %>% 
@@ -256,87 +268,36 @@ mse_evaluation <- train_evaluation_data %>%
 
 
 
-# calculate AUC for test set
+# calculate AUC for train and test set
 library(pROC)
 
-auc_test <- roc(response = test_evaluation_data$Actual, predictor = test_evaluation_data$Prediction)
+#train set
+auc_train <- roc(response = train_evaluation_data$Actual, predictor = train_evaluation_data$Hazard)
+auc_train
+plot(auc_train, legacy.axes = TRUE, print.thres=TRUE, auc.polygon =TRUE,auc.polygon.col = 'lightblue')
+
+
+auc_test <- roc(response = test_evaluation_data$Actual, predictor = test_evaluation_data$Hazard)
 auc_test
 plot(auc_test, legacy.axes = TRUE, print.thres=TRUE, auc.polygon =TRUE,auc.polygon.col = 'lightblue')
 
 
-# 6. Profit error calculation ----
+# 6. profit error calculation ----
 
-# function to generate Interest rates,loss given default and observed profit for calculating profit error
-
-get_profit_data <- function(data){
-  
-  # filter data with missing values in LossGivenDefault
-  data <- data[which(!is.na(data$LossGivenDefault)),]
-  
-  # convert annual Interest rates to monthly interest rate and express in decimal
-  data$Interest = (data$Interest / 12) / 100
-  
-  # Prepare empty data frame to store the data
-  profit_data <- data.frame(observed_profit = numeric(0),I = numeric(0), D = numeric(0))
-  
-  # generate required data                             
-  for (i in 1:nrow(data)){
-    
-    # generate Interest/ Observed-Profits
-    
-    if (data$Status[i] == 1) {
-      observed_profit <- c(rep(data$Interest[i], (data$Time[i]- 1)), - (data$LossGivenDefault[i]))
-      
-    } else {
-      observed_profit <- rep(data$Interest[i], data$Time[i])
-    }
-    
-    # generate interest rates
-    
-    I <- rep(data$Interest[i], data$Time[i])
-    
-    # generate Loss given defaults
-    
-    D <- - rep(data$LossGivenDefault[i],data$Time[i])
-    
-    
-    # create temporary data frame to store the generated data
-    df <- data.frame(observed_profit = observed_profit, I = I, D = D)
-    
-    profit_data <- rbind(profit_data, df)
-  }
-  return(profit_data)
-}
-
-
-# profit evaluation data for train set
-
-train_profit_evaluation <- map_df(.x = folds_train, .f = get_profit_data)
-
-# combine train_profit_evaluation  with train_evaluation data column wise
-
-train_profit_evaluation <- bind_cols(train_evaluation_data, train_profit_evaluation)
-
-#  profit evaluation data for test set
-
-test_profit_evaluation <- map_df(.x = folds_test, .f = get_profit_data)
-
-# combine train_profit_evaluation  with train_evaluation data column wise
-
-test_profit_evaluation <- bind_cols(test_evaluation_data, test_profit_evaluation)
-
-
-# profit error calculation
-
-profit_error <- train_profit_evaluation %>% 
+profit_error <- train_evaluation_data %>% 
   
   #row bind train and test profit evaluation data
-  bind_rows(test_profit_evaluation) %>% 
+  bind_rows(test_evaluation_data) %>% 
   
   # calculate the expected profit
-  mutate(expected_profit = (Prediction * I ) + ((1-Prediction) * D)) %>% 
+  mutate(expected_profit = ((1 - Hazard) * I)  + (Hazard * D)) %>% 
   
-  # square error of profit 
+  #annual profit
+  mutate(expected_profit_yearly = ((expected_profit+1)^12 - 1) * 100)
+
+
+# square error of profit 
+profit_error %>% 
   mutate(squared_error = (observed_profit - expected_profit)^2) %>% 
   
   # mean square error of profit for train and test set
@@ -347,15 +308,161 @@ profit_error <- train_profit_evaluation %>%
 
 # 7. profit estimation on test set
 
-profit_estimation <- test_data %>% 
-  filter(!is.na(LossGivenDefault)) %>% 
+test_data <- test_data %>% 
   mutate (h = hazard,
           D = - LossGivenDefault,
-          I = (Interest/12)/100) %>% 
+          I = (Interest/12)/100) %>%
   mutate(profit = (h*(1+I)*(1+D)+(1-h)*(1+I))-1 ,
-         profit_yearly = ((profit+1)^12 - 1) * 100) %>% 
-  select(hazard,profit_yearly)
+         expected_profit = ((profit+1)^12 - 1) * 100)
 
 
+# histogram of the expected yearly profit and interest on test set
+profit_estimation <- test_data %>% 
+  select(Interest,hazard,Interest, expected_profit) %>% 
+  gather(key = Type, value = Profit, -hazard)
+
+library(tidyquant)
+ggplot(profit_estimation, aes(x = Profit, col = 'red1'))+
+  geom_histogram(fill = 'lightgreen',bins = 50, show.legend = F)+
+  scale_x_continuous( breaks = seq(-100,200, by = 20))+ 
+  facet_wrap(~Type, scales = 'free_x')+
+  labs(title = 'Comparison of expected profit and Interest rates on loans',
+       x = 'Return in percentage')+theme_tq()
+  
+
+# Plot of observed profit and expected profit
+profit_error %>% 
+  sample_n(30000) %>% 
+  ggplot(aes(x=expected_profit, y= observed_profit, color = as.factor(Actual)))+
+  geom_point()+geom_jitter()+theme_tq()
+
+ # Residual plot
+ profit_error %>% 
+  # residual of profit 
+  mutate(residual = (observed_profit - expected_profit)) %>% 
+  ggplot(aes(x = expected_profit, y= residual))+geom_point() +
+   geom_hline(yintercept =0, color = 'red')
+  
+ 
+# plot of interest and hazard
+ test_data %>% 
+   ggplot(aes(x= hazard, y = Interest))+geom_point(color='royalblue1', shape=21)+
+   scale_color_hue(l=30, c= 20)+
+   labs(x='Haxard rates (%)',
+        y = 'Interest rates (%)')+
+   theme_tq()
+
+# Distribution of Expected profit in the Ratings groups 
+test_data %>% 
+  ggplot(aes(x = fct_relevel(Rating, "AA"), y = expected_profit))+
+  geom_boxplot(show.legend = FALSE)+
+  scale_y_continuous(breaks = seq(-80, 100, by =15))+
+  labs( x = 'Rating',
+        y = 'Expected_Profit (%)')+
+  theme_tq()
+  
+ 
+
+# 8. loan selection ----
+   
+# Selection of loans based on Ratings
+ 
+selection_rating <- data.frame(Rating = factor(), expected_profit = numeric(0),
+                               selection = character(0))  
+
+for (i in seq(0.1,1, by =0.1)){
+  
+  select_data <- test_data %>%
+    mutate(Rating = fct_relevel(Rating,"AA")) %>% 
+    arrange(Rating) %>% 
+    select(Rating, expected_profit) %>% 
+    slice(1 : (i *nrow(test_data))) %>% 
+    mutate(selection = paste0('top_', i*100 ))
+    
+  selection_rating <- bind_rows(selection_rating,select_data)
+  
+}
+ 
+
+# Selection of loans based on hazard rates
+
+selection_hazard <- data.frame(hazard = numeric(0),expected_profit = numeric(0),
+                               selection = character(0))  
+
+for (i in seq(0.1,1, by =0.1)){
+  
+  select_data <- test_data %>%
+    arrange(hazard) %>% 
+    select(hazard,expected_profit) %>% 
+    slice(1 : (i *nrow(test_data))) %>% 
+    mutate(selection = paste0('top_', i*100 ))
+  
+  selection_hazard <- bind_rows(selection_hazard,select_data)
+  
+}
+ 
+
+# Selection of loans based on expected profit
+
+selection_profit <- data.frame(Rating = factor(), expected_profit = numeric(0),
+                               selection = character(0))  
+
+for (i in seq(0.1,1, by =0.1)){
+  
+  select_data <- test_data %>%
+    arrange(desc(expected_profit)) %>% 
+    select(Rating, expected_profit) %>% 
+    slice(1 : (i *nrow(test_data))) %>% 
+    mutate(selection = paste0('top_', i*100 ))
+  
+  selection_profit<- bind_rows(selection_profit,select_data)
+  
+}
+
+
+# column bind selcetion_rating, selection_hazard and selection_profit
+
+selection_combine <- bind_cols(selection_rating, selection_hazard, selection_profit)
+
+selection_combine <- selection_combine %>% 
+  select(-Rating, -hazard,-Rating1,-selection1, -selection2 ) %>% 
+  rename(expected_profit_rating = expected_profit,
+         expected_profit_hazard = expected_profit1,
+         expected_profit_profits =expected_profit2) %>% 
+  group_by(selection) %>% 
+  summarise(Rating = mean(expected_profit_rating),
+            Hazard = mean(expected_profit_hazard),
+            Expected_profit = mean(expected_profit_profits)) %>% 
+  gather(key = selection_type, value = avg_profit, -selection)
+
+# plot the avg return based on different selection criteria
+
+selection_combine %>% 
+  mutate(selection = as.factor(selection),
+         selection = fct_relevel(selection, "top_100", after = 9)) %>% 
+  mutate(selection_type = as.factor(selection_type),
+         selection_type = fct_relevel(selection_type, "Rating", "Hazard","Expected_profit")) %>% 
+  ggplot(aes(x = avg_profit, y = selection))+
+  geom_point()+
+  scale_x_continuous(breaks = seq(0,40, by= 5))+
+  geom_segment(aes(xend = 0, yend = selection))+
+  facet_wrap(~ selection_type)+
+  geom_label(aes(label = round(avg_profit,2),color = selection_type, hjust='inward'),
+             show.legend = F)+
+  scale_color_manual(values = c( "black","blue","brown3"))+
+  theme_tq()+
+  labs(title = "Average Predicted Expected Profit based on Different selection Criteria",
+       x = "Aveage Profit (%)",
+       y = "")+
+  theme(title = element_text(size = 10))
+
+
+
+
+selection_profit %>% 
+  mutate_if(is.character, as_factor) %>% 
+  ggplot(aes(x = selection))+
+  geom_bar(aes(fill = Rating),color = 'black', position = 'fill')+
+  scale_fill_brewer(type = 'div', palette = 'Set3', direction = 1)
 
 
